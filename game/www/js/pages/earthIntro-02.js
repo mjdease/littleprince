88620f7dda578bf13ab1b242bee282e553ab863a
andongko <andongko@gmail.com>
2013-12-03 20:38:44 -0500

    Added asteroids menu graphics

--- game/www/js/pages/earthIntro-02.js
+++ game/www/js/pages/earthIntro-02.js
@@ -5,18 +5,37 @@
 
     page.setNextPage("earthIntro", 3);
 
-    page.setNarration("assets/sound/test.mp3");
+    page.setNarration("assets/sound/test.mp3"),
+
+   page.setRequiredAssets([
+    //{name: "background", path: "assets/images/earthIntro/page1.jpg"} 
+    {name: "spritePage3", path: "assets/images/earthIntro/spritePage3.jpg"}
+]);
+
 
     page.initPage = function(images, stage, layers){
+        // sprite animation for page
+        sprite = storybook.defineSprite({
+            x:0,
+            y:0,
+            image: images.spritePage3,
+            animation: "testAnim",
+            frameRate: 1
+        }, 1280, 800, {testAnim: 4});
+        layers.dynBack.add(sprite);
 
     };
 
     page.startPage = function(){
+        sprite.start();
 
+        page.setState(page.States.PLAYING);
     };
 
     page.update = function(frame, stage, layers){
-
+        if(page.getState() != page.States.PLAYING){
+            return;
+        }
     };
 
     storybook.registerPage(page);
