84d060b5b3313ad63fe8387e64f3c7dd6fceb8f7
Matt Dease <mattdease+dev@gmail.com>
2013-12-20 09:25:38 -0500

    Add status message, score display, changed rate of stars, and changed victory/added loss condition to stars challenge.

--- game/www/js/pages/earthEnding-08.js
+++ game/www/js/pages/earthEnding-08.js
@@ -1,9 +1,13 @@
+// TODO - refine
 (function(){
+    var speed = 50;
 
     var redStars = 10;
     var yellowStars = 10;
     var blueStars = 10;
     var purpleStars = 10;
+
+    var count = 0;
 
     var starRDisplay = new Array();
     var starYDisplay = new Array();
@@ -35,6 +39,12 @@
     var starsBCrossed = new Boolean (0);
     var starsPCrossed = new Boolean (0);
 
+    var starWidth = 32;
+    var barWidth;
+    var stageWidth, stageHeight;
+
+    var scoreTxt;
+
     var sounds = {};
 
     var page = new Page("earthEnding", 8);
@@ -61,8 +71,23 @@
     page.setNarration();
 
     page.initPage = function(images, stage, layers){
+        stageWidth = stage.getWidth();
+        stageHeight = stage.getHeight();
 
         //sounds.soundeffect = new Sound("assets/sound/test.mp3", false, false);
+
+        scoreTxt = new Kinetic.Text({
+            fontFamily:"lp_BodyFont",
+            fontSize:32,
+            stroke:"white",
+            strokeWidth:1,
+            align:"center",
+            fill:"white",
+            y:400,
+            x:stageWidth/2,
+            width:200,
+            offsetX:100
+        });
 
         playByBar = new Kinetic.Image({
             x: 400,
@@ -73,6 +98,7 @@
         });
 
         playByPos = playByBar.getX();
+        barWidth = playByBar.getWidth();
 
         starDisplayBar = new Kinetic.Image({
             x: 0,
@@ -84,21 +110,24 @@
 
         layers.staticBack.add(starDisplayBar);
         layers.staticBack.add(playByBar);
+        layers.dynBack.add(scoreTxt);
 
         starRClick = storybook.defineSprite({
             x: 400,
             y: 100,
             image: images.starRClick,
             animation: "starRClickAnim",
-            frameRate: 2
+            frameRate: 2,
+            offset: {x:68,y:68}
         }, 136, 136, {starRClickAnim: 2});
 
         starYClick = storybook.defineSprite({
             x: 500,
-            y: 50,
+            y: 150,
             image: images.starYClick,
             animation: "starYClickAnim",
-            frameRate: 2
+            frameRate: 2,
+            offset: {x:68,y:68}
         }, 136, 136, {starYClickAnim: 2});
 
         starGClick = storybook.defineSprite({
@@ -106,7 +135,8 @@
             y: 140,
             image: images.starGClick,
             animation: "starGClickAnim",
-            frameRate: 2
+            frameRate: 2,
+            offset: {x:68,y:68}
         }, 136, 136, {starGClickAnim: 2});
 
         starPClick = storybook.defineSprite({
@@ -114,7 +144,8 @@
             y: 70,
             image: images.starPClick,
             animation: "starPClickAnim",
-            frameRate: 2
+            frameRate: 2,
+            offset: {x:68,y:68}
         }, 136, 136, {starPClickAnim: 2});
 
         layers.dynFront.add(starRClick);
@@ -125,8 +156,8 @@
         for(var r = 0; r < redStars; r++)
         {
             starRDisplay[r] = new Kinetic.Image({
-            x: randomInt(-1000, 0),
-            y: randomInt(540, 660),
+            x: -2500 + r*300,
+            y: 540,
             image: images.starRDisplay,
             width: 32,
             height: 32
@@ -138,8 +169,8 @@
         for(var y = 0; y < yellowStars; y++)
         {
             starYDisplay[y] = new Kinetic.Image({
-            x: randomInt(-1000, 0),
-            y: randomInt(540, 660),
+            x: -2500 + 150 + y * 300,
+            y: 580,
             image: images.starYDisplay,
             width: 32,
             height: 32
@@ -151,8 +182,8 @@
         for(var b = 0; b < blueStars; b++)
         {
             starBDisplay[b] = new Kinetic.Image({
-            x: randomInt(-1000, 0),
-            y: (540, 660),
+            x: -2500 + 75 + b * 175,
+            y: 620,
             image: images.starBDisplay,
             width: 32,
             height: 32
@@ -164,14 +195,14 @@
         for(var p = 0; p < purpleStars; p++)
         {
             starPDisplay[p] = new Kinetic.Image({
-            x: randomInt(-1000, 0),
-            y: (540, 660),
+            x: -2500 + 225 + y * 225,
+            y: 660,
             image: images.starPDisplay,
             width: 32,
             height: 32
             });
 
-            layers.dynFront.add(starBDisplay[p]);
+            layers.dynFront.add(starPDisplay[p]);
         }
 
 
@@ -215,36 +246,55 @@
     }
 
     page.update = function(frame, stage, layers){
+        if(page.getState() != page.States.PLAYING){
+            return;
+        }
 
         for(var r = 0; r < redStars; r++)
         {
-            if (((starRDisplay[r].getX() + starRDisplay[r].getWidth()) > playByPos) && (starRDisplay[r].getX() + (playByBar.getWidth()/2)) < (playByPos + playByBar.getWidth()))
-
-            {
+            var starX = starRDisplay[r].getX();
+            if (Math.abs(starX - playByPos) < 32){
                 sDRAtBar = 1;
+                if(!starRDisplay[r].passed){
+                    count++;
+                    starRDisplay[r].passed = true;
+                }
             }
         }
 
         for(var y = 0; y < yellowStars; y++)
         {
-            if(((starYDisplay[y].getX() + starYDisplay[y].getWidth()) > playByPos) && (starYDisplay[y].getX() + (playByBar.getWidth()/2)) < (playByPos + playByBar.getWidth()))            {
+            var starX = starYDisplay[y].getX();
+            if (Math.abs(starX - playByPos) < 32){
                 sDYAtBar = 1;
+                if(!starYDisplay[y].passed){
+                    count++;
+                    starYDisplay[y].passed = true;
+                }
             }
         }
 
         for(var b = 0; b < blueStars; b++)
         {
-            if(((starBDisplay[b].getX() + starBDisplay[b].getWidth()) > playByPos) && (starRDisplay[b].getX() + (playByBar.getWidth()/2)) < (playByPos + playByBar.getWidth()))
-            {
+            var starX = starBDisplay[b].getX();
+            if (Math.abs(starX - playByPos) < 32){
                 sDBAtBar = 1;
+                if(!starBDisplay[b].passed){
+                    count++;
+                    starBDisplay[b].passed = true;
+                }
             }
         }
 
         for(var p = 0; p < purpleStars; p++)
         {
-            if(((starPDisplay[p].getX() + starPDisplay[p].getWidth()) > playByPos) && (starRDisplay[p].getX() + (playByBar.getWidth()/2)) < (playByPos + playByBar.getWidth()))
-            {
+            var starX = starPDisplay[p].getX();
+            if (Math.abs(starX - playByPos) < 32){
                 sDPAtBar = 1;
+                if(!starPDisplay[p].passed){
+                    count++;
+                    starPDisplay[p].passed = true;
+                }
             }
         }
 
@@ -252,45 +302,37 @@
         if(sCRClicked == 1)
         {
             starRClick.setScale(1.5);
-            starRClick.setOffset(36,36);
         }
         else
         {
             starRClick.setScale(1);
-            starRClick.setOffset(0,0);
         }
 
         if(sCYClicked == 1)
         {
            starYClick.setScale(1.5);
-           starYClick.setOffset(36,36);
         }
         else
         {
             starYClick.setScale(1);
-            starYClick.setOffset(0,0);
         }
 
         if(sCBClicked == 1)
         {
            starGClick.setScale(1.5);
-           starGClick.setOffset(36,36);
         }
         else
         {
            starGClick.setScale(1);
-           starGClick.setOffset(0,0);
         }
 
         if(sCPClicked == 1)
         {
            starPClick.setScale(1.5);
-           starPClick.setOffset(36,36);
         }
         else
         {
            starPClick.setScale(1);
-           starPClick.setOffset(0,0);
         }
 
 
@@ -304,7 +346,6 @@
             else
             {
                 //play error sound
-                console.log("oops");
                 score-= 1;
             }
         }
@@ -318,7 +359,6 @@
             else
             {
                 //play error sound
-                console.log("oops");
                 score-= 1;
             }
         }
@@ -331,7 +371,6 @@
             else
             {
                 //play error sound
-                console.log("oops");
                 score-= 1;
             }
         }
@@ -345,10 +384,11 @@
             else
             {
                 //play error sound
-                console.log("oops");
                 score-= 1;
             }
         }
+
+        scoreTxt.setText("Score: " + score);
 
         sCRClicked = 0;
         sCYClicked = 0;
@@ -366,85 +406,140 @@
 
         //if((starRCrossed == 1) && (starYCrossed ==1) && (starBCrossed == 1) && (starPCrossed == 1))
         //{
-        // console.log("all stars have crossed");
-            if (score >= (redStars + yellowStars + blueStars + purpleStars)/4)
+        if(count >= 40){
+            if (score >= 15)
             {
-                 page.challengeComplete();
+                 endChallenge(true, "You played the notes!", layers.staticFront);
             }
             else
             {
-                //console.log("failed mission");
+                endChallenge(false, "You missed to many notes.", layers.staticFront);
             }
-            //}
-
-
-        console.log(score);
-
-        if(page.getState() != page.States.PLAYING){
-            return;
         }
-
+            //}
+        var dispX = speed * frame.timeDiff /1000;
         for(var r = 0; r < redStars; r++)
         {
-            var starRMove = starRDisplay[r].getX() + 5.5;
-            starRDisplay[r].setX(starRMove);
+            starRDisplay[r].move(dispX, 0);
         }
 
         for(var y = 0; y < yellowStars; y++)
         {
-            var starYMove = starYDisplay[y].getX() + 5.5;
-            starYDisplay[y].setX(starYMove);
+            starYDisplay[y].move(dispX, 0);
         }
 
         for(var b = 0; b < blueStars; b++)
         {
-            var starBMove = starBDisplay[b].getX() + 5.5;
-            starBDisplay[b].setX(starBMove);
+            starBDisplay[b].move(dispX, 0);
         }
 
         for(var p = 0; p < purpleStars; p++)
         {
-            var starPMove = starPDisplay[p].getX() + 5.5;
-            starPDisplay[p].setX(starPMove);
+            starPDisplay[p].move(dispX, 0);
         }
     };
 
     page.destroyPage = function(){
+        resetChallenge();
+        // for(var b = 0; b < blueStars; b++)
+        // {
+        //     starBDisplay[b].off(clickEvt);
+        // }
 
-        for(var b = 0; b < blueStars; b++)
-        {
-            starBDisplay[b].off(clickEvt);
-        }
+        // for(var y = 0; y < yellowStars; y++)
+        // {
+        //     starYDisplay[y].off(clickEvt);
+        // }
 
-        for(var y = 0; y < yellowStars; y++)
-        {
-            starYDisplay[y].off(clickEvt);
-        }
+        // for(var r = 0; r < redStars; r++)
+        // {
+        //     starRDisplay[r].off(clickEvt);
+        // }
 
-        for(var r = 0; r < redStars; r++)
-        {
-            starRDisplay[r].off(clickEvt);
-        }
+        // for(var p = 0; p < purpleStars; r++)
+        // {
+        //     starPDisplay[p].off(clickEvt);
+        // }
 
-        for(var p = 0; p < purpleStars; r++)
-        {
-            starPDisplay[p].off(clickEvt);
-        }
-
-        starBClick.off(clickEvt);
-        starYClick.off(clickEvt);
-        starRClick.off(clickEvt);
-        starPClick.off(clickEvt);
-
+        // starBClick.off(clickEvt);
+        // starYClick.off(clickEvt);
+        // starRClick.off(clickEvt);
+        // starPClick.off(clickEvt);
 
         //sounds.soundeffect.destroy();
         //delete sounds.soundeffect;
     };
 
-    function onSpriteClick(e){
+    function endChallenge(isPass, message, layer){
+        var msgbox = new Kinetic.Rect({
+            x:stageWidth/2,
+            y:stageHeight/2,
+            width:900,
+            height: 72,
+            offsetX:450,
+            offsetY: 36,
+            fill: isPass ? "green" : "red",
+            opacity: 0.8,
+            stroke: "black",
+            strokeWeight: 10
+        });
+        var msg = new Kinetic.Text({
+            text:message,
+            fontFamily:"lp_BodyFont",
+            fontWeight:"bold",
+            fontSize:32,
+            padding:20,
+            align:"center",
+            fill:"black",
+            x:stageWidth/2,
+            y:stageHeight/2,
+            width:900,
+            height: 72,
+            offsetX:450,
+            offsetY: 36,
+            listening:false
+        });
+        if(isPass){
+            page.setState(page.States.PASSED);
+        }
+        else{
+            page.setState(page.States.FAILED);
+        }
+        layer.add(msgbox).add(msg).batchDraw();
+    }
 
-        //sounds.soundeffect.play();
+    function resetChallenge(){
+        sCRClicked = 0;
+        sCYClicked = 0;
+        sCBClicked = 0;
+        sCPClicked = 0;
+        sDRAtBar = 0;
+        sDYAtBar = 0;
+        sDBAtBar = 0;
+        sDPAtBar = 0;
 
+        score = 0;
+        count = 0;
+
+        // for(var r = 0; r < redStars; r++)
+        // {
+        //     starRDisplay[r].passed = false;
+        // }
+
+        // for(var y = 0; y < yellowStars; y++)
+        // {
+        //     starYDisplay[y].passed = false;
+        // }
+
+        // for(var b = 0; b < blueStars; b++)
+        // {
+        //     starBDisplay[b].passed = false;
+        // }
+
+        // for(var p = 0; p < purpleStars; p++)
+        // {
+        //     starPDisplay[p].passed = false;
+        // }
     }
 
     storybook.registerPage(page);
